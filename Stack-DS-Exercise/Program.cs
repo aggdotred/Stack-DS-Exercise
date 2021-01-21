489c0d86f0769c67cccf329799f4607815a07331
Adam Gutierrez <adam.gutierrez@hyland.com>
2021-01-21 16:30:41 -0600

    Add project files.

---
+++ Stack-DS-Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stack_DS_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push("Hi");
+            Console.WriteLine(stack.Pop());
+            stack.Push("HiHi");
+            stack.Push("Hello");
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            stack.Clear();
+        }
+    }
+}
---
+++ Stack-DS-Exercise/Stack.cs
@@ -0,0 +1,22 @@
+﻿namespace Stack_DS_Exercise
+{
+    class Stack 
+    {
+        object[] Arr = new object[1];
+        public void Push(object obj) 
+        {
+            Arr[Arr.Length - 1] = obj;
+            
+        }
+        public object Pop() 
+        { 
+            object obj = Arr[Arr.Length - 1];
+            Arr[Arr.Length - 1] = null;
+            return obj;
+        }
+        public void Clear() 
+        {
+            Arr = new object[1];
+        }
+    }
+}
